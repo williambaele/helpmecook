719eab9a1c5c83953f40ce06d6b0b2c2883ab6d6
William Baele <baelewilliam1999@gmail.com>
2023-02-22 18:04:49 +0100

    Added log in/log out method on root

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+  add_flash_types :info, :error, :success
+  def after_sign_up_path_for(resource)
+    sign_in(resource)
+    flash[:success] = "Account created and signed in"
+    root_path
+  end
+
+  def after_sign_in_path_for(resource)
+    flash[:success] = "Logged in"
+    root_path
+  end
 end
