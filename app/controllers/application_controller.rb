920a6899beb05745d05f623a9354bc55888fa9da
William Baele <baelewilliam1999@gmail.com>
2023-02-23 11:37:48 +0100

    Fixed account update error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,10 +3,10 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   def configure_permitted_parameters
     # For additional fields in app/views/devise/registrations/new.html.erb
-    devise_parameter_sanitizer.permit(:sign_up, keys: %i[pseudo last_name first_name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[last_name first_name pseudo])
 
     # For additional in app/views/devise/registrations/edit.html.erb
-    devise_parameter_sanitizer.permit(:account_update, keys: %i[pseudo last_name first_name])
+    devise_parameter_sanitizer.permit(:account_update, keys: %i[last_name first_name pseudo])
   end
   def after_sign_up_path_for(resource)
     sign_in(resource)
@@ -14,7 +14,7 @@ class ApplicationController < ActionController::Base
     root_path
   end
   def after_update_path_for(resource)
-    root_path
+    edit_user_registration_path
     flash[:success] = "Your account has been updated successfully."
   end
 
