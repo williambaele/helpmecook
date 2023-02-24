50180abdd4bbe21245aa43dc67294e256c52ac63
William Baele <baelewilliam1999@gmail.com>
2023-02-24 18:05:45 +0100

    Fixed recipe's model data verification

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,5 +1,8 @@
 class Recipe < ApplicationRecord
-  validates :title, :description, :people, presence: true
+  validates :title, :description, :people, :time, :budget, :recipe_type, presence: true
+  validates :descritpion, length: { minimum: 50, maximum: 500 }
+  validates :title, length: { minimum: 10, maximum: 100 }
+
   belongs_to :user
   include PgSearch::Model
   pg_search_scope :search_by_title_description,
