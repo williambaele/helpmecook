288d03b75938950995863cabd516fe1643105de5
William Baele <baelewilliam1999@gmail.com>
2023-03-07 11:37:29 +0100

    Fixed for cloudinary settings

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,4 @@
 require_relative "boot"
-require 'dotenv/load'
-
 require "rails/all"
 
 # Require the gems listed in Gemfile, including any gems
---
+++ config/initializers/cloudinary.rb
@@ -0,0 +1,6 @@
+Cloudinary.config do |config|
+  config.cloud_name = 'di12n1ept'
+  config.api_key = '375886329227568'
+  config.api_secret = 'rXR_ueowM5O1K2eYHk8Ut1CqWD0'
+  config.secure = true
+end
