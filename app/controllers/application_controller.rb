f23e81d06713cdbb44b79cb23244e372e35ee3ec
William Baele <baelewilliam1999@gmail.com>
2023-03-07 12:00:49 +0100

    Added profile picture hosting

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,10 +3,10 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   def configure_permitted_parameters
     # For additional fields in app/views/devise/registrations/new.html.erb
-    devise_parameter_sanitizer.permit(:sign_up, keys: %i[last_name first_name pseudo])
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[last_name first_name pseudo photo])
 
     # For additional in app/views/devise/registrations/edit.html.erb
-    devise_parameter_sanitizer.permit(:account_update, keys: %i[last_name first_name pseudo])
+    devise_parameter_sanitizer.permit(:account_update, keys: %i[last_name first_name pseudo photo])
   end
   def after_sign_up_path_for(resource)
     sign_in(resource)
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 has_many :recipes
 has_many :comments
+has_one_attached :photo
 validates :first_name, :last_name, length: { minimum: 2, maximum: 50 }
 validates :pseudo, length: { minimum: 3, maximum: 15 }
 validates :first_name, :last_name, :email, :pseudo, presence: true
